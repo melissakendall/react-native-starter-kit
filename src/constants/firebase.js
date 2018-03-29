eb92a4318909755c5d38a9827f5562eb11cc160f
Melissa Kendall <MelissaK@iQmetrix.com>
2018-03-29 10:22:45 -0600

    Initial commit. If this gets committed to wrong rep due to fork, I apologise

--- src/constants/firebase.js
+++ src/constants/firebase.js
@@ -1,8 +1,8 @@
 export default {
-  apiKey: 'AIzaSyAHshJehJ-E_9AoUN3kfY8BLvZtFoXxFto',
-  authDomain: 'react-native-starter-app.firebaseapp.com',
-  databaseURL: 'https://react-native-starter-app.firebaseio.com',
-  projectId: 'react-native-starter-app',
-  storageBucket: 'react-native-starter-app.appspot.com',
-  messagingSenderId: '762822550667',
+  apiKey: 'AIzaSyCYVv5Z1x366g0wPu9aFKi_zkqbe7IYTWU',
+  authDomain: 'react-native-starter-app-1945a.firebaseapp.com',
+  databaseURL: 'https://react-native-starter-app-1945a.firebaseio.com',
+  projectId: 'react-native-starter-app-1945a',
+  storageBucket: 'react-native-starter-app-1945a.appspot.com',
+  messagingSenderId: '8174801037',
 };
